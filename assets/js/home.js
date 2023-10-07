4b39a84efd1eb3fd23a56eadc75db80868ca7dcd
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-10-07 12:33:54 -0700

    Increment numbers using javascript

---
+++ assets/js/home.js
@@ -0,0 +1,54 @@
+const stats = document.querySelectorAll(".home4-stats-amt");
+const initStats = [];
+stats.forEach(stat => initStats.push(stat.innerHTML));
+console.log(Number(initStats.find(stat => stat.startsWith("$")).replace("$", "")));
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            if (entry.target.innerHTML.startsWith("$")) {
+
+                let dollars = 0;
+                entry.target.innerHTML = `$${dollars.toLocaleString()}`;
+                let target = Number(initStats.find(stat => stat.startsWith("$")).replace("$", "").replace(",", ""));
+
+                const incrementDollars = setInterval(() => {
+                    dollars = Math.min(dollars + 50, target);
+                    entry.target.innerHTML = `$${dollars.toLocaleString()}`;
+                    if (dollars === target) {
+                        clearInterval(incrementDollars);
+                    }
+                }, 3);
+            } else if (entry.target.innerHTML.endsWith("+")) {
+
+                let volunteers = 0;
+                entry.target.innerHTML = `${volunteers.toLocaleString()}+`;
+                let target = Number(initStats.find(stat => stat.endsWith("+")).replace("+", ""));
+
+                const incrementVolunteers = setInterval(() => {
+                    volunteers = Math.min(volunteers + 1, target);
+                    entry.target.innerHTML = `${volunteers.toLocaleString()}+`;
+                    if (volunteers === target) {
+                        clearInterval(incrementVolunteers);
+                    }
+                }, 15);
+            } else {
+
+                let events = 0;
+                entry.target.innerHTML = `${events.toLocaleString()}`;
+                let target = Number(initStats.find(stat => !stat.endsWith("+") && !stat.startsWith("$")));
+
+                const incrementEvents = setInterval(() => {
+                    events = Math.min(events + 1, target);
+                    entry.target.innerHTML = `${events.toLocaleString()}`;
+                    if (events === target) {
+                        clearInterval(incrementEvents);
+                    }
+                }, 180);
+            }
+        }
+    })
+});
+
+stats.forEach(stat => observer.observe(stat));
