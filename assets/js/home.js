4f3c003b050f273e386b1897b9831e971b83e948
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-10-08 16:00:19 -0700

    Make increment more consistent

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,46 +5,52 @@ stats.forEach(stat => initStats.push(stat.innerHTML));
 const statsObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
+            const targetedCompletionMs = 300;
 
             if (entry.target.innerHTML.startsWith("$")) {
+                const increment = 100;
 
                 let dollars = 0;
                 entry.target.innerHTML = `$${dollars.toLocaleString()}`;
                 let target = Number(initStats.find(stat => stat.startsWith("$")).replace("$", "").replace(",", ""));
 
                 const incrementDollars = setInterval(() => {
-                    dollars = Math.min(dollars + 50, target);
+                    dollars = Math.min(dollars + increment, target);
                     entry.target.innerHTML = `$${dollars.toLocaleString()}`;
+
                     if (dollars === target) {
                         clearInterval(incrementDollars);
                     }
-                }, 3);
+                    // Timeout is the number of milliseconds between each increment, so that total increments take targetedCompletionMs
+                }, targetedCompletionMs / (target / increment));
             } else if (entry.target.innerHTML.endsWith("+")) {
+                const increment = 1;
 
                 let volunteers = 0;
                 entry.target.innerHTML = `${volunteers.toLocaleString()}+`;
                 let target = Number(initStats.find(stat => stat.endsWith("+")).replace("+", ""));
 
                 const incrementVolunteers = setInterval(() => {
-                    volunteers = Math.min(volunteers + 1, target);
+                    volunteers = Math.min(volunteers + increment, target);
                     entry.target.innerHTML = `${volunteers.toLocaleString()}+`;
                     if (volunteers === target) {
                         clearInterval(incrementVolunteers);
                     }
-                }, 15);
+                }, targetedCompletionMs / (target / increment));
             } else {
+                const increment = 1;
 
                 let events = 0;
                 entry.target.innerHTML = `${events.toLocaleString()}`;
                 let target = Number(initStats.find(stat => !stat.endsWith("+") && !stat.startsWith("$")));
 
                 const incrementEvents = setInterval(() => {
-                    events = Math.min(events + 1, target);
+                    events = Math.min(events + increment, target);
                     entry.target.innerHTML = `${events.toLocaleString()}`;
                     if (events === target) {
                         clearInterval(incrementEvents);
                     }
-                }, 180);
+                }, targetedCompletionMs / (target / increment));
             }
         }
     })
