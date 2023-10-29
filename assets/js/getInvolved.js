96a3785a4919e0b1d4fc84c79966d83fbfd4f23c
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-10-29 08:15:09 -0700

    Complete most of past-events cards

--- assets/js/getInvolved.js
+++ assets/js/getInvolved.js
@@ -1,10 +1,14 @@
 // TODO THE FOLLOWING CODE HAS NOT BEEN TESTED. TEST BEFORE USE!
 const expandEventsButton = document.getElementById("expandPastEvents");
 expandEventsButton.addEventListener("click", () => {
-
     const hiddenPastEvents = document.getElementsByClassName("hiddenPastEvent");
-    hiddenPastEvents.forEach(e => {
+    /*
+    Note that we do not use a traditional for-of loop because the length of the array changes as we remove elements.
+    If we were to use an iterator regardless, we would be skipping over elements.
+    */
+    while (hiddenPastEvents.length) {
+        const e = hiddenPastEvents[0];
         e.classList.remove("hiddenPastEvent");
-    });
+    }
     expandEventsButton.classList.add("hidden");
 }, false);
