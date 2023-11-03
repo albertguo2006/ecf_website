11684c1a54a27ce87e3d7298414a28b92115d348
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-11-03 09:12:36 -0700

    Remove todo

--- assets/js/getInvolved.js
+++ assets/js/getInvolved.js
@@ -1,10 +1,9 @@
-// TODO THE FOLLOWING CODE HAS NOT BEEN TESTED. TEST BEFORE USE!
 const expandEventsButton = document.getElementById("expandPastEvents");
 expandEventsButton.addEventListener("click", () => {
     const hiddenPastEvents = document.getElementsByClassName("hiddenPastEvent");
     /*
     Note that we do not use a traditional for-of loop because the length of the array changes as we remove elements.
-    If we were to use an iterator regardless, we would be skipping over elements.
+    If we were to use a loop regardless, we would be skipping over elements.
     */
     while (hiddenPastEvents.length) {
         const e = hiddenPastEvents[0];
