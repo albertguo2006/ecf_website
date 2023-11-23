9e0b732e70ff4049ecda4be0de6295bb01ea1ac6
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-11-23 13:55:45 -0800

    Add prettier

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,31 +1,32 @@
 // We don't use the shadow DOM because we want styles to leak into the custom elements
-customElements.define("ecf-wrapper", class extends HTMLElement {
-    constructor() {
-        super();
-    }
-
-    // This is used, unlike what the IDE might claim
-    connectedCallback() {
-
-        // Check for dev environment
-        let prefix = window.location.pathname.startsWith("/ecf_website") ? "/ecf_website" : "";
-        let currentPath = window.location.pathname;
-
-        // Only include the first part of the path after the prefix, prefix included.
-        const pathEnd = currentPath.indexOf("/", prefix !== "" ? prefix.length + 1 : 1);
-        if (pathEnd !== -1) {
-            currentPath = currentPath.substring(0, pathEnd + 1);
-        }
-        // Remove index.html
-        if (currentPath.indexOf("index.html") !== -1) {
-            currentPath = currentPath.substring(0, currentPath.indexOf("index.html"));
-        }
-        // Add trailing slash if not present
-        if (currentPath.endsWith("/") === false) {
-            currentPath += "/";
+customElements.define(
+    "ecf-wrapper",
+    class extends HTMLElement {
+        constructor() {
+            super()
         }
 
-        const navHTML = `
+        // This is used, unlike what the IDE might claim
+        connectedCallback() {
+            // Check for dev environment
+            let prefix = window.location.pathname.startsWith("/ecf_website") ? "/ecf_website" : ""
+            let currentPath = window.location.pathname
+
+            // Only include the first part of the path after the prefix, prefix included.
+            const pathEnd = currentPath.indexOf("/", prefix !== "" ? prefix.length + 1 : 1)
+            if (pathEnd !== -1) {
+                currentPath = currentPath.substring(0, pathEnd + 1)
+            }
+            // Remove index.html
+            if (currentPath.indexOf("index.html") !== -1) {
+                currentPath = currentPath.substring(0, currentPath.indexOf("index.html"))
+            }
+            // Add trailing slash if not present
+            if (currentPath.endsWith("/") === false) {
+                currentPath += "/"
+            }
+
+            const navHTML = `
         <nav>
             <a href=${prefix === "" ? "/" : `${prefix}/`}>
                 <img class="nav__logo" src="${prefix}/assets/icon.ico" alt="Evergreen Collective's logo" loading="lazy">
@@ -57,9 +58,9 @@ customElements.define("ecf-wrapper", class extends HTMLElement {
                 <li><a href="https://blog.evergreencollective.ca/" target="_blank" rel="noopener noreferrer">BLOG</a></li>
                 <li><a href="https://www.paypal.com/paypalme/ExpeditionPeng" target="_blank" rel="noopener noreferrer">DONATE</a></li>
             </ul>
-        </nav>`;
+        </nav>`
 
-        const footerHTML = `
+            const footerHTML = `
         <footer>
             <div class="footer__contact"><h1>CONTACT US</h1></div>
             <div class="footer__socials">
@@ -68,27 +69,28 @@ customElements.define("ecf-wrapper", class extends HTMLElement {
                 <a href="https://www.instagram.com/evergreencollect/"><i class="fa-brands fa-instagram fa-2xl"></i></a>
                 <a href="https://ca.linkedin.com/company/evergreencollective"><i class="fa-brands fa-linkedin-in fa-2xl"></i></a>
             </div>
-        </footer>`;
+        </footer>`
 
-        // For some reason, the class content needs to be applied to both the wrapper and the content div
-        this.innerHTML = `${navHTML}<div class="content">${this.innerHTML}</div>${footerHTML}`;
-        this.classList.add("content");
+            // For some reason, the class content needs to be applied to both the wrapper and the content div
+            this.innerHTML = `${navHTML}<div class="content">${this.innerHTML}</div>${footerHTML}`
+            this.classList.add("content")
 
-        // Add the current indicator to the appropriate nav element
-        const navList = document.getElementById("nav-list");
-        // Use the innerHTML so that a user can still click the link if they click the indicator
-        navList.querySelector(`a[href="${currentPath}"]`).innerHTML += '<div class="nav__current-indicator"></div>';
-    }
-});
+            // Add the current indicator to the appropriate nav element
+            const navList = document.getElementById("nav-list")
+            // Use the innerHTML so that a user can still click the link if they click the indicator
+            navList.querySelector(`a[href="${currentPath}"]`).innerHTML += '<div class="nav__current-indicator"></div>'
+        }
+    },
+)
 
-const toLazyLoad = document.querySelectorAll(".lazy-load");
-const lazyLoaderObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
+const toLazyLoad = document.querySelectorAll(".lazy-load")
+const lazyLoaderObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
         // No point in doing anything if the element is hidden anyway. Shouldn't happen, but jic
-        if (!entry.isIntersecting || entry.target.hidden) return;
-        entry.target.classList.remove("lazy-load");
-        lazyLoaderObserver.unobserve(entry.target);
+        if (!entry.isIntersecting || entry.target.hidden) return
+        entry.target.classList.remove("lazy-load")
+        lazyLoaderObserver.unobserve(entry.target)
     })
-});
+})
 
-toLazyLoad.forEach(element => lazyLoaderObserver.observe(element));
+toLazyLoad.forEach((element) => lazyLoaderObserver.observe(element))
--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,27 +1,29 @@
 // Correctly identify what post is selected on page load, if any
 window.onload = () => {
-    const fragment = window.location.hash;
+    const fragment = window.location.hash
     // Deselect all selected posts
-    document.querySelectorAll(".current-post").forEach(post => {
-        post.classList.remove("current-post");
-        post.classList.add("not-current-post");
-    });
-    const inactiveSelectors = document.querySelectorAll(".not-current-post");
-    const inactiveSelector = (fragment && fragment !== "#home-3-post-1" && fragment.startsWith("#home-3-post-")) ?
-        inactiveSelectors[Number(fragment.replace("#home-3-post-", "")) - 1] : inactiveSelectors[0];
-    inactiveSelector.classList.remove("not-current-post");
-    inactiveSelector.classList.add("current-post");
+    document.querySelectorAll(".current-post").forEach((post) => {
+        post.classList.remove("current-post")
+        post.classList.add("not-current-post")
+    })
+    const inactiveSelectors = document.querySelectorAll(".not-current-post")
+    const inactiveSelector =
+        fragment && fragment !== "#home-3-post-1" && fragment.startsWith("#home-3-post-") ?
+            inactiveSelectors[Number(fragment.replace("#home-3-post-", "")) - 1]
+        :   inactiveSelectors[0]
+    inactiveSelector.classList.remove("not-current-post")
+    inactiveSelector.classList.add("current-post")
 }
 
 // Change post selector colour (implemented using css) on click
-document.onclick = e => {
+document.onclick = (e) => {
     if (e.target.classList.contains("not-current-post")) {
-        document.querySelectorAll(".current-post").forEach(post => {
-            post.classList.remove("current-post");
-            post.classList.add("not-current-post");
-        });
-        e.target.classList.add("current-post");
-        e.target.classList.remove("not-current-post");
+        document.querySelectorAll(".current-post").forEach((post) => {
+            post.classList.remove("current-post")
+            post.classList.add("not-current-post")
+        })
+        e.target.classList.add("current-post")
+        e.target.classList.remove("not-current-post")
     }
 }
 
@@ -29,77 +31,77 @@ document.onclick = e => {
 const initStats = {
     // Stats may need to be formatted so that we get numbers without errors
 
-    "dollars": Number(document.getElementById("home-4-stats-dollars").innerHTML.replace("$", "").replace(",", "")),
-    "volunteers": Number(document.getElementById("home-4-stats-volunteers").innerHTML.replace("+", "")),
-    "events": Number(document.getElementById("home-4-stats-events").innerHTML)
+    dollars: Number(document.getElementById("home-4-stats-dollars").innerHTML.replace("$", "").replace(",", "")),
+    volunteers: Number(document.getElementById("home-4-stats-volunteers").innerHTML.replace("+", "")),
+    events: Number(document.getElementById("home-4-stats-events").innerHTML),
 }
 
-const statsObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) return;
+const statsObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return
 
-        const targetedCompletionMs = 300;
+        const targetedCompletionMs = 300
         // The undefined fields need to be defined in the switch statement, else the rest of the code will break
-        let target = undefined;
-        let increment = undefined;
-        let current = 0;
+        let target = undefined
+        let increment = undefined
+        let current = 0
 
         /*
         Timeout is the number of milliseconds between each increment, so that total increments take targetedCompletionMs.
         Calculation is timout = totalTime / speed
          */
-        const timeout = () => targetedCompletionMs / (target / increment);
+        const timeout = () => targetedCompletionMs / (target / increment)
 
         switch (entry.target.id) {
             case "home-4-stats-dollars":
-                target = initStats["dollars"];
-                increment = 100;
+                target = initStats["dollars"]
+                increment = 100
 
-                entry.target.innerHTML = `$${current.toLocaleString()}`;
+                entry.target.innerHTML = `$${current.toLocaleString()}`
 
                 const incrementDollars = setInterval(() => {
                     // This ensures that we don't exceed target
-                    current = Math.min(current + increment, target);
-                    entry.target.innerHTML = `$${current.toLocaleString()}`;
-
-                    if (current !== target) return;
-                    clearInterval(incrementDollars);
-                }, timeout());
-                break;
-
-            case "home-4-stats-volunteers":
-                target = initStats["volunteers"];
-                increment = 1;
-
-                entry.target.innerHTML = `${current.toLocaleString()}+`;
-
-                const incrementVolunteers = setInterval(() => {
-                    current = Math.min(current + increment, target);
-                    entry.target.innerHTML = `${current.toLocaleString()}+`;
+                    current = Math.min(current + increment, target)
+                    entry.target.innerHTML = `$${current.toLocaleString()}`
 
                     if (current !== target) return
-                    clearInterval(incrementVolunteers);
-                }, timeout());
-                break;
+                    clearInterval(incrementDollars)
+                }, timeout())
+                break
+
+            case "home-4-stats-volunteers":
+                target = initStats["volunteers"]
+                increment = 1
+
+                entry.target.innerHTML = `${current.toLocaleString()}+`
+
+                const incrementVolunteers = setInterval(() => {
+                    current = Math.min(current + increment, target)
+                    entry.target.innerHTML = `${current.toLocaleString()}+`
+
+                    if (current !== target) return
+                    clearInterval(incrementVolunteers)
+                }, timeout())
+                break
 
             case "home-4-stats-events":
-                target = initStats["events"];
-                increment = 1;
+                target = initStats["events"]
+                increment = 1
 
-                entry.target.innerHTML = current.toLocaleString();
+                entry.target.innerHTML = current.toLocaleString()
 
                 const incrementEvents = setInterval(() => {
-                    current = Math.min(current + increment, target);
-                    entry.target.innerHTML = current.toLocaleString();
+                    current = Math.min(current + increment, target)
+                    entry.target.innerHTML = current.toLocaleString()
 
-                    if (current !== target) return;
-                    clearInterval(incrementEvents);
-                }, timeout());
-                break;
+                    if (current !== target) return
+                    clearInterval(incrementEvents)
+                }, timeout())
+                break
         }
     })
-});
+})
 
-statsObserver.observe(document.getElementById("home-4-stats-dollars"));
-statsObserver.observe(document.getElementById("home-4-stats-volunteers"));
-statsObserver.observe(document.getElementById("home-4-stats-events"));
+statsObserver.observe(document.getElementById("home-4-stats-dollars"))
+statsObserver.observe(document.getElementById("home-4-stats-volunteers"))
+statsObserver.observe(document.getElementById("home-4-stats-events"))
